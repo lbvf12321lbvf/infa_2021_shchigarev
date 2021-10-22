3a91ccd439a6c394edce4ac0f49ca8e4b669d407
lbvf12321lbvf <shchigarev.da@phystech.edu>
2021-10-22 19:45:56 +0300

    произведён небольшой рефакторинг

--- lab3/gun.py
+++ lab3/gun.py
@@ -23,7 +23,7 @@ def on_key_press(event):
     elif event.keysym == 's':
         vy = 5
     if event.keysym == '1' or '2':
-        g1.shoot_type(event.keysym)
+        g1.shot_type(event.keysym)
 
 
 def on_key_release(event):
@@ -34,7 +34,7 @@ def on_key_release(event):
         vy = 0
 
 
-class board():
+class Board:
     def __init__(self, x, y, hx, hy):
         self.x = x
         self.y = y
@@ -48,29 +48,29 @@ class board():
         y = obj.y
         x = obj.x
         r = obj.r
-        if isinstance(obj, ball):
+        if isinstance(obj, Balls):
             self.k = 3 / 4
-        if isinstance(obj, target):
-            if obj.type == 'carier':
+        if isinstance(obj, Target):
+            if obj.type == 'carrier':
                 self.kx = 2
 
         if self.hy == 0:
-            if (self.y + r) >= y and (self.y - r) <= y and self.x < x and self.x + self.hx > x:
-                obj.y = self.y + (r + 1) * np.sign(obj.vy)
-                obj.vy = - obj.vy * self.k
-                obj.vx *= self.k
+            if (self.y + r) >= y >= (self.y - r) and self.x < x < self.x + self.hx:
+                obj.y = self.y + (r + 1) * np.sign(obj.v_y)
+                obj.v_y = - obj.v_y * self.k
+                obj.v_x *= self.k
         if self.hx == 0:
-            if (self.x + r * self.kx) >= x and (self.x - r * self.kx) <= x and self.y < y and self.y + self.hy > y:
-                obj.x = self.x - (r * self.kx + 1) * np.sign(obj.vx)
-                obj.vx = - obj.vx * self.k
-                obj.vy *= self.k
+            if (self.x + r * self.kx) >= x >= (self.x - r * self.kx) and self.y < y < self.y + self.hy:
+                obj.x = self.x - (r * self.kx + 1) * np.sign(obj.v_x)
+                obj.v_x = - obj.v_x * self.k
+                obj.v_y *= self.k
         if obj.x >= 1000 or obj.y >= 1000 or obj.y <= -100 or obj.y <= -100:
             obj.live = - 1
         self.k = 1
         self.kx = 1
 
 
-class ball():
+class Balls:
     def __init__(self, x=40, y=450):
         """ Конструктор класса ball
 
@@ -132,7 +132,7 @@ class ball():
             canv.delete(self.id)
         self.set_coords()
 
-    def hittest(self, obj):
+    def hit_test(self, obj):
         """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
 
         Args:
@@ -147,11 +147,11 @@ class ball():
             return False
 
 
-class gun():
+class Gun:
     def __init__(self):
-        self.f2_power = 5
+        self.power = 5
         self.type = 'ball'
-        self.f2_on = 0
+        self.on = 0
         self.an = 1
         self.ou = 1
         self.vx = 0
@@ -169,16 +169,16 @@ class gun():
             fill=self.color
         )
 
-    def shoot_type(self, x):
+    def shot_type(self, x):
         if x == '1':
             self.type = 'ball'
             print(1)
         if x == '2':
-            self.type = 'shootgan'
+            self.type = 'shotgun'
             print(2)
 
     def fire2_start(self, event):
-        self.f2_on = 1
+        self.on = 1
 
     def fire2_end(self, event):
         """Выстрел мячом.
@@ -189,42 +189,42 @@ class gun():
         global balls, bullet
         bullet += 1
         if self.type == 'ball':
-            new_ball = ball(self.x, self.y)
+            new_ball = Balls(self.x, self.y)
             new_ball.r += 5
             if (event.x - self.x) >= 0:
                 self.an = math.atan((event.y - self.y) / (event.x - self.x))
             else:
                 self.an = - math.atan((event.y - self.y) / (event.x - self.x))
             if (event.x - self.x) >= 0:
-                new_ball.vx = self.f2_power * math.cos(self.an)
-                new_ball.vy = - self.f2_power * math.sin(self.an)
+                new_ball.vx = self.power * math.cos(self.an)
+                new_ball.vy = - self.power * math.sin(self.an)
             else:
-                new_ball.vx = - self.f2_power * math.cos(self.an)
-                new_ball.vy = - self.f2_power * math.sin(self.an)
+                new_ball.vx = - self.power * math.cos(self.an)
+                new_ball.vy = - self.power * math.sin(self.an)
             balls += [new_ball]
-            self.f2_on = 0
-            self.f2_power = 10
-        if self.type == 'shootgan':
+            self.on = 0
+            self.power = 10
+        if self.type == 'shotgun':
             for i in range(3):
-                new_ball = ball(self.x, self.y)
+                new_ball = Balls(self.x, self.y)
                 new_ball.r -= 2
                 if (event.x - self.x) >= 0:
                     self.an = math.atan((event.y - self.y) / (event.x - self.x))
                 else:
                     self.an = - math.atan((event.y - self.y) / (event.x - self.x))
                 if (event.x - self.x) >= 0:
-                    new_ball.vx = self.f2_power * math.cos(self.an)
-                    new_ball.vy = - self.f2_power * math.sin(self.an)
+                    new_ball.vx = self.power * math.cos(self.an)
+                    new_ball.vy = - self.power * math.sin(self.an)
                 else:
-                    new_ball.vx = - self.f2_power * math.cos(self.an)
-                    new_ball.vy = - self.f2_power * math.sin(self.an)
+                    new_ball.vx = - self.power * math.cos(self.an)
+                    new_ball.vy = - self.power * math.sin(self.an)
                 new_ball.rot(-0.3 + 0.3 * i)
                 balls += [new_ball]
 
-            self.f2_on = 0
-            self.f2_power = 10
+            self.on = 0
+            self.power = 10
 
-    def targetting(self, event=0):
+    def targeting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
             if (event.x - self.x) >= 0:
@@ -235,36 +235,36 @@ class gun():
                 self.ou = -1
             if (event.x - self.x) >= 0:
                 canv.coords(self.id, self.x, self.y,
-                            self.x + max(self.f2_power, 20) * math.cos(self.an),
-                            self.y + max(self.f2_power, 20) * math.sin(self.an)
+                            self.x + max(self.power, 20) * math.cos(self.an),
+                            self.y + max(self.power, 20) * math.sin(self.an)
                             )
             else:
                 canv.coords(self.id, self.x, self.y,
-                            self.x - max(self.f2_power, 20) * math.cos(self.an),
-                            self.y + max(self.f2_power, 20) * math.sin(self.an)
+                            self.x - max(self.power, 20) * math.cos(self.an),
+                            self.y + max(self.power, 20) * math.sin(self.an)
                             )
         else:
             if self.ou == 1:
                 canv.coords(self.id, self.x, self.y,
-                            self.x + max(self.f2_power, 20) * math.cos(self.an),
-                            self.y + max(self.f2_power, 20) * math.sin(self.an)
+                            self.x + max(self.power, 20) * math.cos(self.an),
+                            self.y + max(self.power, 20) * math.sin(self.an)
                             )
             else:
                 canv.coords(self.id, self.x, self.y,
-                            self.x - max(self.f2_power, 20) * math.cos(self.an),
-                            self.y + max(self.f2_power, 20) * math.sin(self.an)
+                            self.x - max(self.power, 20) * math.cos(self.an),
+                            self.y + max(self.power, 20) * math.sin(self.an)
                             )
 
-        if self.f2_on:
+        if self.on:
             canv.itemconfig(self.id, fill='orange')
         else:
             canv.itemconfig(self.id, fill='black')
 
-    def move(self, vx, vy):
-        self.vx = vx
-        self.vy = -vy
-        self.x += vx
-        self.y += vy
+    def move(self, v_x, v_y):
+        self.vx = v_x
+        self.vy = -v_y
+        self.x += v_x
+        self.y += v_y
         canv.coords(
             self.id2,
             self.x - self.r,
@@ -276,20 +276,20 @@ class gun():
             bord.check(self)
 
     def power_up(self):
-        if self.f2_on:
-            if self.f2_power < 50:
-                self.f2_power += 0.5
+        if self.on:
+            if self.power < 50:
+                self.power += 0.5
             canv.itemconfig(self.id, fill='orange')
         else:
             canv.itemconfig(self.id, fill='black')
 
 
-class target():
-    def __init__(self, typ='standart'):
+class Target():
+    def __init__(self, typ='standard'):
         self.points = 0
         self.live = 1
         self.type = typ
-        if self.type == 'standart' or self.type == 'carier':
+        if self.type == 'standard' or self.type == 'carrier':
             self.id = canv.create_oval(0, 0, 0, 0)
         elif self.type == 'rare':
             self.id = canv.create_rectangle(0, 0, 0, 0)
@@ -297,15 +297,15 @@ class target():
 
     def new_target(self):
         """ Инициализация новой цели. """
-        if self.type == 'standart':
+        if self.type == 'standard':
             x = self.x = rnd(600, 780)
             y = self.y = rnd(300, 550)
             r = self.r = rnd(7, 50)
             color = self.color = 'red'
             canv.coords(self.id, x - r, y - r, x + r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.vy = rnd(-10, 10)
-            self.vx = rnd(-10, 10)
+            self.v_y = rnd(-10, 10)
+            self.v_x = rnd(-10, 10)
         elif self.type == 'rare':
             x = self.x = rnd(600, 780)
             y = self.y = rnd(300, 550)
@@ -313,23 +313,23 @@ class target():
             color = self.color = 'blue'
             canv.coords(self.id, x - r, y - r, x + r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.vy = rnd(-15, 15)
-            self.vx = rnd(-15, 15)
-        elif self.type == 'carier':
+            self.v_y = rnd(-15, 15)
+            self.v_x = rnd(-15, 15)
+        elif self.type == 'carrier':
             x = self.x = rnd(600, 780)
             y = self.y = rnd(300, 550)
             r = self.r = rnd(20, 30)
             color = self.color = 'green'
             canv.coords(self.id, x - 2 * r, y - r, x + 2 * r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.vy = rnd(-5, 5)
-            self.vx = rnd(-5, 5)
+            self.v_y = rnd(-5, 5)
+            self.v_x = rnd(-5, 5)
 
     def move(self, boards):
         for bord in boards:
             bord.check(self)
-        self.x += self.vx
-        self.y -= self.vy
+        self.x += self.v_x
+        self.y -= self.v_y
 
         if self.live <= 0:
             canv.delete(self.id)
@@ -337,7 +337,7 @@ class target():
             self.set_coords()
 
     def set_coords(self):
-        if self.type != 'carier':
+        if self.type != 'carrier':
             canv.coords(
                 self.id,
                 self.x - self.r,
@@ -361,11 +361,11 @@ class target():
             self.points += 2 * points
         canv.delete(self.id)
 
-    def dele(self):
+    def delete_new(self):
         canv.delete(self.id)
 
 
-class texty():
+class Texts:
     def __init__(self):
         self.id = canv.create_text(30, 30, text='', font='28')
 
@@ -374,96 +374,91 @@ class texty():
         self.id = canv.create_text(30, 30, text=str(sc), font='28')
 
 
-t1 = defaultdict(lambda: '')
-tr = defaultdict(lambda: '')
-tc = defaultdict(lambda: '')
+t_standard = defaultdict(lambda: Target())
+t_rare = defaultdict(lambda: Target())
+t_carrier = defaultdict(lambda: Target())
 screen1 = canv.create_text(400, 300, text='', font='28')
-g1 = gun()
+g1 = Gun()
 bullet = 0
 score = 0
 vx = 0
 vy = 0
 balls = []
-t1[0] = target()
-t1[0].dele()
+t_standard[0] = Target()
+t_standard[0].delete_new()
 
-tx = texty()
-boards = []
-boards.append(board(4, 4, 800, 0))
-boards.append(board(4, 596, 800, 0))
-boards.append(board(4, 4, 0, 600))
-boards.append(board(796, 4, 0, 600))
-boards.append(board(300, 4, 0, 200))
-boards.append(board(500, 400, 0, 200))
+tx = Texts()
+boards = [Board(4, 4, 800, 0), Board(4, 596, 800, 0), Board(4, 4, 0, 600), Board(796, 4, 0, 600), Board(300, 4, 0, 200),
+          Board(500, 400, 0, 200)]
 
 
-def new_game(N, event=''):
-    global gun, t1, screen1, balls, bullet, score
-    for i in range(N):
-        t1[i + 1] = target()
-        t1[i + 1].new_target()
-    for i in range(N - 1):
-        tr[i] = target(typ='rare')
-        tr[i].new_target()
-    for i in range(N - 1):
-        tc[i] = target(typ='carier')
-        tc[i].new_target()
+def new_game(n):
+    global t_standard, screen1, balls, bullet, score
+    for i in range(n):
+        t_standard[i + 1] = Target()
+        t_standard[i + 1].new_target()
+    for i in range(n - 1):
+        t_rare[i] = Target(typ='rare')
+        t_rare[i].new_target()
+    for i in range(n - 1):
+        t_carrier[i] = Target(typ='carrier')
+        t_carrier[i].new_target()
     bullet = 0
     balls = []
     canv.bind('<Button-1>', g1.fire2_start)
     canv.bind('<ButtonRelease-1>', g1.fire2_end)
-    canv.bind('<Motion>', g1.targetting)
-    canv.bind('<Motion>', g1.targetting)
-    canv.bind('<Motion>', g1.targetting)
+    canv.bind('<Motion>', g1.targeting)
+    canv.bind('<Motion>', g1.targeting)
+    canv.bind('<Motion>', g1.targeting)
     root.bind('<KeyPress>', on_key_press)
     root.bind('<KeyRelease>', on_key_release)
 
-    z = 0.01
-    l = 0
+    tick = 0.01
+    successful_targets = 0
     num = 0
-    t1[0].live = 1
-    while t1[0].live or balls:
+    t_standard[0].live = 1
+    while t_standard[0].live or balls:
         for b in balls:
             b.move(boards)
-            for i in range(N):
-                if b.hittest(t1[i + 1]):
-                    t1[i + 1].live = 0
-                    t1[i + 1].hit()
-                    l += 1
-            for i in range(N - 1):
-                if b.hittest(tr[i]):
-                    tr[i].live = 0
-                    tr[i].hit()
-                    l += 1
-            for i in range(N - 1):
-                if b.hittest(tc[i]):
-                    tc[i].live = 0
-                    tc[i].hit()
-                    l += 1
-            if l == 3 * N - 2 + num:
+            for i in range(n):
+                if b.hit_test(t_standard[i + 1]):
+                    t_standard[i + 1].live = 0
+                    t_standard[i + 1].hit()
+                    successful_targets += 1
+            for i in range(n - 1):
+                if b.hit_test(t_rare[i]):
+                    t_rare[i].live = 0
+                    t_rare[i].hit()
+                    successful_targets += 1
+            for i in range(n - 1):
+                if b.hit_test(t_carrier[i]):
+                    t_carrier[i].live = 0
+                    t_carrier[i].hit()
+                    successful_targets += 1
+            if successful_targets == 3 * n - 2 + num:
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
                 canv.itemconfig(screen1, text='Вы уничтожили цели за ' + str(bullet) + ' выстрелов')
-                t1[0].live = 0
+                t_standard[0].live = 0
         score = 0
-        for i in range(N):
-            t1[i + 1].move(boards)
-            score += t1[i + 1].points
-        for i in range(N - 1):
-            tr[i].move(boards)
-            score += tr[i].points
-        for i in range(N - 1):
-            tc[i].move(boards)
-            score += tc[i].points
+        for i in range(n):
+            t_standard[i + 1].move(boards)
+            score += t_standard[i + 1].points
+        for i in range(n - 1):
+            t_rare[i].move(boards)
+            score += t_rare[i].points
+        for i in range(n - 1):
+            t_carrier[i].move(boards)
+            score += t_carrier[i].points
         tx.peretext(score)
         canv.update()
-        time.sleep(z)
+        time.sleep(tick)
         g1.move(vx, vy)
-        g1.targetting()
+        g1.targeting()
         g1.power_up()
     canv.itemconfig(screen1, text='')
-    canv.delete(gun)
-    root.after(750, new_game(N + 1))
+    canv.delete(Gun)
+    root.after(750, new_game(n + 1))
 
 
 new_game(1)
