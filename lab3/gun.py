e073925021204e05bbe873fcbeb0d9ea13fb3147
lbvf12321lbvf <shchigarev.da@phystech.edu>
2021-10-28 04:31:17 +0300

    исправлены многие ошибки, добавлены жизни и спавн овалам

--- lab3/gun.py
+++ lab3/gun.py
@@ -45,6 +45,7 @@ class Board:
         self.kx = 1
 
     def check(self, obj, typ=''):
+        global successful_targets
         y = obj.y
         x = obj.x
         r = obj.r
@@ -56,15 +57,18 @@ class Board:
 
         if self.hy == 0:
             if (self.y + r) >= y >= (self.y - r) and self.x < x < self.x + self.hx:
-                obj.y = self.y + (r + 1) * np.sign(obj.v_y)
-                obj.v_y = - obj.v_y * self.k
-                obj.v_x *= self.k
+                obj.y = self.y + (r + 1) * np.sign(obj.vy)
+                obj.vy = - obj.vy * self.k
+                obj.vx *= self.k
         if self.hx == 0:
             if (self.x + r * self.kx) >= x >= (self.x - r * self.kx) and self.y < y < self.y + self.hy:
-                obj.x = self.x - (r * self.kx + 1) * np.sign(obj.v_x)
-                obj.v_x = - obj.v_x * self.k
-                obj.v_y *= self.k
+                obj.x = self.x - (r * self.kx + 1) * np.sign(obj.vx)
+                obj.vx = - obj.vx * self.k
+                obj.vy *= self.k
         if obj.x >= 1000 or obj.y >= 1000 or obj.y <= -100 or obj.y <= -100:
+            if isinstance(obj, Target) and obj.live > 0:
+                successful_targets += 1
+                print('lol')
             obj.live = - 1
         self.k = 1
         self.kx = 1
@@ -141,7 +145,7 @@ class Balls:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
 
-        if abs(obj.x - self.x) <= (self.r + obj.r) and abs(obj.y - self.y) <= (self.r + obj.r) and obj.live == 1:
+        if abs(obj.x - self.x) <= (self.r + obj.r) and abs(obj.y - self.y) <= (self.r + obj.r) and obj.live >= 1:
             return True
         else:
             return False
@@ -150,6 +154,8 @@ class Balls:
 class Gun:
     def __init__(self):
         self.power = 5
+        self.live = 3
+        self.inviz_time = 0
         self.type = 'ball'
         self.on = 0
         self.an = 1
@@ -160,6 +166,7 @@ class Gun:
         self.x = 20
         self.y = 450
         self.color = choice(['blue', 'green', 'red', 'brown'])
+        self.color2 = self.color
         self.id = canv.create_line(20, 450, 50, 420, width=7)
         self.id2 = canv.create_oval(
             self.x - self.r,
@@ -168,6 +175,12 @@ class Gun:
             self.y + self.r,
             fill=self.color
         )
+
+    def minus_live(self):
+        if self.inviz_time <= 0:
+            self.live -= 1
+            self.color = 'white'
+            self.inviz_time = 100
 
     def shot_type(self, x):
         if x == '1':
@@ -224,6 +237,20 @@ class Gun:
             self.on = 0
             self.power = 10
 
+    def hit_test(self, obj):
+        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
+
+        Args:
+            obj: Обьект, с которым проверяется столкновение.
+        Returns:
+            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
+        """
+
+        if abs(obj.x - self.x) <= (self.r + obj.r) and abs(obj.y - self.y) <= (self.r + obj.r) and obj.live >= 1:
+            return True
+        else:
+            return False
+
     def targeting(self, event=0):
         """Прицеливание. Зависит от положения мыши."""
         if event:
@@ -270,10 +297,16 @@ class Gun:
             self.x - self.r,
             self.y - self.r,
             self.x + self.r,
-            self.y + self.r
+            self.y + self.r,
         )
         for bord in boards:
             bord.check(self)
+
+    def chek_color(self):
+        self.inviz_time -= 1
+        if self.inviz_time <= 0:
+            self.color = self.color2
+        canv.itemconfig(self.id2, fill=self.color)
 
     def power_up(self):
         if self.on:
@@ -284,52 +317,53 @@ class Gun:
             canv.itemconfig(self.id, fill='black')
 
 
-class Target():
+class Target:
     def __init__(self, typ='standard'):
         self.points = 0
         self.live = 1
         self.type = typ
+        self.tik = 100
         if self.type == 'standard' or self.type == 'carrier':
             self.id = canv.create_oval(0, 0, 0, 0)
         elif self.type == 'rare':
             self.id = canv.create_rectangle(0, 0, 0, 0)
         self.new_target()
 
-    def new_target(self):
+    def new_target(self, x1=rnd(600, 759), y1=rnd(300, 550)):
         """ Инициализация новой цели. """
         if self.type == 'standard':
-            x = self.x = rnd(600, 780)
+            x = self.x = rnd(600, 750)
             y = self.y = rnd(300, 550)
             r = self.r = rnd(7, 50)
             color = self.color = 'red'
             canv.coords(self.id, x - r, y - r, x + r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.v_y = rnd(-10, 10)
-            self.v_x = rnd(-10, 10)
+            self.vy = rnd(-10, 10)
+            self.vx = rnd(-10, 10)
         elif self.type == 'rare':
-            x = self.x = rnd(600, 780)
-            y = self.y = rnd(300, 550)
+            x = self.x = x1
+            y = self.y = y1
             r = self.r = rnd(7, 15)
             color = self.color = 'blue'
             canv.coords(self.id, x - r, y - r, x + r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.v_y = rnd(-15, 15)
-            self.v_x = rnd(-15, 15)
+            self.vy = rnd(-15, 15)
+            self.vx = rnd(-15, 15)
         elif self.type == 'carrier':
-            x = self.x = rnd(600, 780)
+            x = self.x = rnd(600, 750)
             y = self.y = rnd(300, 550)
             r = self.r = rnd(20, 30)
             color = self.color = 'green'
             canv.coords(self.id, x - 2 * r, y - r, x + 2 * r, y + r)
             canv.itemconfig(self.id, fill=color)
-            self.v_y = rnd(-5, 5)
-            self.v_x = rnd(-5, 5)
+            self.vy = rnd(-5, 5)
+            self.vx = rnd(-5, 5)
 
     def move(self, boards):
         for bord in boards:
             bord.check(self)
-        self.x += self.v_x
-        self.y -= self.v_y
+        self.x += self.vx
+        self.y -= self.vy
 
         if self.live <= 0:
             canv.delete(self.id)
@@ -356,26 +390,36 @@ class Target():
     def hit(self, points=1):
         """Попадание шарика в цель."""
         canv.coords(self.id, -10, -10, -10, -10)
-        self.points += points
+        self.points = points
         if self.type == 'rare':
-            self.points += 2 * points
+            self.points = 3 * points
         canv.delete(self.id)
+
+    def spawn(self, n):
+        global t_rare, num
+
+        if self.live > 0 and self.tik <= 0:
+            num += 1
+            t_rare[n - 2 + num] = Target(typ='rare')
+            t_rare[n - 2 + num].new_target(self.x, self.y)
+            self.tik = 50
+        self.tik -= 1
 
     def delete_new(self):
         canv.delete(self.id)
 
 
 class Texts:
-    def __init__(self):
-        self.id = canv.create_text(30, 30, text='', font='28')
+    def __init__(self, x=30, y=30, tex=''):
+        self.id = canv.create_text(x, y, text=tex, font='28')
 
-    def peretext(self, sc):
+    def peretext(self, sc, x=30, y=30, tex=''):
         canv.delete(self.id)
-        self.id = canv.create_text(30, 30, text=str(sc), font='28')
+        self.id = canv.create_text(x, y, text=(tex + str(sc)), font='28')
 
 
 t_standard = defaultdict(lambda: Target())
-t_rare = defaultdict(lambda: Target())
+t_rare = defaultdict(lambda: Target(typ='rare'))
 t_carrier = defaultdict(lambda: Target())
 screen1 = canv.create_text(400, 300, text='', font='28')
 g1 = Gun()
@@ -383,17 +427,21 @@ bullet = 0
 score = 0
 vx = 0
 vy = 0
+k = 2
+tik = 100
 balls = []
 t_standard[0] = Target()
 t_standard[0].delete_new()
-
+successful_targets = 0
+num = 0
 tx = Texts()
+tx_live = Texts(x=70, y=30, tex='live: 0')
 boards = [Board(4, 4, 800, 0), Board(4, 596, 800, 0), Board(4, 4, 0, 600), Board(796, 4, 0, 600), Board(300, 4, 0, 200),
           Board(500, 400, 0, 200)]
 
 
 def new_game(n):
-    global t_standard, screen1, balls, bullet, score
+    global t_standard, screen1, balls, bullet, score, successful_targets, num, tik, k
     for i in range(n):
         t_standard[i + 1] = Target()
         t_standard[i + 1].new_target()
@@ -405,6 +453,9 @@ def new_game(n):
         t_carrier[i].new_target()
     bullet = 0
     balls = []
+    g1.live = 3
+    k = 3
+
     canv.bind('<Button-1>', g1.fire2_start)
     canv.bind('<ButtonRelease-1>', g1.fire2_end)
     canv.bind('<Motion>', g1.targeting)
@@ -417,7 +468,7 @@ def new_game(n):
     successful_targets = 0
     num = 0
     t_standard[0].live = 1
-    while t_standard[0].live or balls:
+    while (t_standard[0].live or balls) and k > 0:
         for b in balls:
             b.move(boards)
             for i in range(n):
@@ -425,7 +476,7 @@ def new_game(n):
                     t_standard[i + 1].live = 0
                     t_standard[i + 1].hit()
                     successful_targets += 1
-            for i in range(n - 1):
+            for i in range(n - 1 + num):
                 if b.hit_test(t_rare[i]):
                     t_rare[i].live = 0
                     t_rare[i].hit()
@@ -435,7 +486,8 @@ def new_game(n):
                     t_carrier[i].live = 0
                     t_carrier[i].hit()
                     successful_targets += 1
-            if successful_targets == 3 * n - 2 + num:
+
+            if successful_targets == 3 * n - 2 + num and n > 0:
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
                 canv.itemconfig(screen1, text='Вы уничтожили цели за ' + str(bullet) + ' выстрелов')
@@ -444,18 +496,41 @@ def new_game(n):
         for i in range(n):
             t_standard[i + 1].move(boards)
             score += t_standard[i + 1].points
-        for i in range(n - 1):
+        for i in range(n - 1 + num):
             t_rare[i].move(boards)
             score += t_rare[i].points
         for i in range(n - 1):
             t_carrier[i].move(boards)
             score += t_carrier[i].points
+            t_carrier[i].spawn(n)
+
+        for i in range(n - 1 + num):
+            if g1.hit_test(t_rare[i]):
+                g1.minus_live()
+        tx_live.peretext(x=70, y=30, sc=str(g1.live), tex='live:')
+        if g1.live == 0:
+            canv.itemconfig(screen1, text='you lose, score:' + str(score))
+            for i in range(n):
+                t_standard[i + 1].live = 0
+                t_standard[i + 1].hit()
+
+            for i in range(n - 1 + num):
+                t_rare[i].live = 0
+                t_rare[i].hit()
+
+            for i in range(n - 1):
+                t_carrier[i].live = 0
+                t_carrier[i].hit()
+            t_standard[0].live = 0
+            n = 0
         tx.peretext(score)
         canv.update()
         time.sleep(tick)
         g1.move(vx, vy)
         g1.targeting()
         g1.power_up()
+        g1.chek_color()
+
     canv.itemconfig(screen1, text='')
     canv.delete(Gun)
     root.after(750, new_game(n + 1))
